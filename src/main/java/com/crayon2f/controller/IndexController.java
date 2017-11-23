f213085b23bd21afd2cdfb6c195d522b88028fd6
feifan.gou <gou_feifan@126.com>
2017-11-23 18:41:53 +0800

    add new gradle

--- src/main/java/com/crayon2f/controller/IndexController.java
+++ src/main/java/com/crayon2f/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.crayon2f.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by feiFan.gou on 2017/11/22 18:50.
  */
+@Controller
 public class IndexController {
+
 }
