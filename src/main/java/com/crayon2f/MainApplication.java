635b1573ebd7bcbe361dbb3c2c722f15cce52328
[goufeifan] <[gou_feifan@126.com]>
2017-11-28 00:24:26 +0800

    config mybatis

--- src/main/java/com/crayon2f/MainApplication.java
+++ src/main/java/com/crayon2f/MainApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 /**
  * Created by feiFan.gou on 2017/11/24 17:25.
  */
-@SpringBootApplication(scanBasePackages = {"com.crayon2f"})
+@SpringBootApplication(scanBasePackages = {"com.crayon2f","com.crayon2f.dao"})
 public class MainApplication {
 
     public static void main(String[] args) {
